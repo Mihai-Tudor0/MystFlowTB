63c3c60a3f2f3855a53f5ba27f299af60640d080
Ghenea Mihai-Tudor <137957723+Mihai-Tudor0@users.noreply.github.com>
2026-05-06 11:56:19 +0300

    initialize Retrofit client, API services, and network permissions (#2)
    * Squashed commit of the following:
    
    commit d169919ac58e2f41963499e2800b38eb0995bd35
    Author: Mihai <tudorghenea5@gmail.com>
    Date:   Tue May 5 23:06:07 2026 +0300
    
        Template for Android Studio
    
        Made a project template so that the tools for Android Studio.
    
    * initialize Retrofit client, API services, and network permissions
    
    ---------
    
    Co-authored-by: bestefan1 <balanstefan769@gmail.com>

---
+++ app/src/main/java/com/example/mystflowtb/ApiClient.kt
@@ -0,0 +1,23 @@
+package com.example.mystflowtb
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private const val BASE_URL = "http://10.0.2.2:8000/"
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/example/mystflowtb/ApiService.kt
@@ -0,0 +1,31 @@
+package com.example.mystflowtb
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+data class RegisterRequest(
+    val username: String,
+    val email: String,
+    val password: String
+)
+
+
+
+data class LoginResponse(
+    val status: String,
+    val message: String,
+    val user_id: Int? = null
+)
+
+//interfata
+interface ApiService {
+    @POST("login")
+    suspend fun login(@Body request: LoginRequest): LoginResponse
+    @POST("register")
+    suspend fun register(@Body request: RegisterRequest): LoginResponse
+}
