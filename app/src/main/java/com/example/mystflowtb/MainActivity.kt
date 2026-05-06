8b991d592c5df2e78f7a7c953f1b1b572eaa4504
Ghenea Mihai-Tudor <137957723+Mihai-Tudor0@users.noreply.github.com>
2026-05-06 12:00:16 +0300

    Added Login and Register screens using Jetpack Compose (#3)
    * Squashed commit of the following:
    
    commit d169919ac58e2f41963499e2800b38eb0995bd35
    Author: Mihai <tudorghenea5@gmail.com>
    Date:   Tue May 5 23:06:07 2026 +0300
    
        Template for Android Studio
    
        Made a project template so that the tools for Android Studio.
    
    * Added Login and Register screens using Jetpack Compose.
    
    Implemented state-based navigation between Auth and Home screens.
    
    Integrated API calls for user authentication and registration.
    
    Added basic UI styling with Material3 theme.
    
    ---------
    
    Co-authored-by: Florian978 <27042004f@gmail.com>

--- app/src/main/java/com/example/mystflowtb/MainActivity.kt
+++ app/src/main/java/com/example/mystflowtb/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
+import com.example.mystflowtb.LoginRequest
+import androidx.compose.ui.unit.dp
 import com.example.mystflowtb.ui.theme.MystFlowTBTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,196 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MystFlowTBTheme {
+                var isLoggedIn by remember { mutableStateOf(false) }
+                var isRegistering by remember { mutableStateOf(false) }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    when {
+                        isLoggedIn -> {
+                            HomeScreen(onLogout = { isLoggedIn = false })
+                        }
+
+                        isRegistering -> {
+                            RegisterScreen(
+                                onRegisterSuccess = {
+                                    isRegistering = false
+                                },
+                                onBackToLogin = {
+                                    isRegistering = false
+                                }
+                            )
+                        }
+
+                        else -> {
+                            LoginScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                onLoginSuccess = { isLoggedIn = true },
+                                onGoToRegister = { isRegistering = true })
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun LoginScreen(modifier: Modifier = Modifier, onLoginSuccess: () -> Unit, onGoToRegister: () -> Unit) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        val scope = rememberCoroutineScope()
+        var mesajServer by remember { mutableStateOf("") }
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "MystFlow Login", style = MaterialTheme.typography.headlineMedium)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MystFlowTBTheme {
-        Greeting("Android")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Parolă") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    scope.launch {
+                        try {
+                            val response = ApiClient.apiService.login(
+                                LoginRequest(email = email, password = password)
+                            )
+                            mesajServer = response.message
+                            if (response.status == "success") {
+                                onLoginSuccess()
+                            }
+                        } catch (e: Exception) {
+                            mesajServer = "Eroare: ${e.localizedMessage}"
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Intră în cont")
+            }
+            TextButton(onClick = onGoToRegister) {
+                Text("Nu ai cont? Creează unul nou")
+            }
+            if (mesajServer.isNotEmpty()) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = mesajServer, color = MaterialTheme.colorScheme.primary)
+            }
+
+        }
+    }
+
+    @Composable
+    fun RegisterScreen(onRegisterSuccess: () -> Unit, onBackToLogin: () -> Unit) {
+        var username by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var mesajServer by remember { mutableStateOf("") }
+        val scope = rememberCoroutineScope()
+
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Creează un cont nou", style = MaterialTheme.typography.headlineMedium)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Nume utilizator") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Parolă") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    scope.launch {
+                        try {
+                            val response = ApiClient.apiService.register(
+                                RegisterRequest(username, email, password)
+                            )
+                            mesajServer = response.message
+                            if (response.status == "success") {
+                                onRegisterSuccess()
+                            }
+                        } catch (e: Exception) {
+                            mesajServer = "Eroare: ${e.localizedMessage}"
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Înregistrează-te")
+            }
+
+            TextButton(onClick = onBackToLogin) {
+                Text("Ai deja cont? Loghează-te aici")
+            }
+
+            if (mesajServer.isNotEmpty()) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = mesajServer, color = MaterialTheme.colorScheme.primary)
+            }
+        }
+    }
+
+    @Composable
+    fun HomeScreen(onLogout: () -> Unit) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = "Ai intrat în cont cu succes!",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(onClick = { onLogout() }) {
+                Text("Ieși din cont")
+            }
+        }
     }
 }
