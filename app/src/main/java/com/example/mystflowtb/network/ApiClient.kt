80f7903dccf222819125ee8e365c628ac7abbf2b
bestefan1 <balanstefan769@gmail.com>
2026-06-15 23:36:33 +0300

    Cloud up

--- app/src/main/java/com/example/mystflowtb/network/ApiClient.kt
+++ app/src/main/java/com/example/mystflowtb/network/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://127.0.0.1:8000/"
+    private const val BASE_URL = "https://mystflowtb.onrender.com/"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
